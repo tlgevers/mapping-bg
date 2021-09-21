277352bebc20c347d17490e2af9785e2cbb36524
Trevor Gevers <tlgeverssa@gmail.com>
2021-09-21 06:17:26 -0700

    add proto fields, load to airport type directly from bigquery

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"fmt"
 
+	"net"
+
 	"cloud.google.com/go/bigquery"
-	pb "github.com/tlgevers/mapping-bg/proto/bq/bq"
+	pb "github.com/tlgevers/mapping-bg/proto"
 	"google.golang.org/api/iterator"
+	"google.golang.org/grpc"
 )
 
 const (
@@ -18,39 +21,88 @@ type server struct {
 	pb.UnimplementedFAAAirportDataServer
 }
 
-func (s *server) GetAirportData(ctx context.Context, in *pb.RequestCode) *pb.Airports {
-	fmt.Printf("Received: %v", in.GetName())
-	return &pb.Airports{}
-}
-
-func main() {
-	ctx := context.Background()
-	client, err := bigquery.NewClient(ctx, projectID)
+func (s *server) GetAirportData(ctx context.Context, in *pb.RequestCode) (*pb.Airports, error) {
+	c := context.Background()
+	client, err := bigquery.NewClient(c, projectID)
 	if err != nil {
 		fmt.Println(err)
 	}
 	q := client.Query(`
-		SELECT year, SUM(number) as num
-		FROM ` + "`bigquery-public-data.usa_names.usa_1910_2013`" + `
-		WHERE name = "William"
-		GROUP BY year
-		ORDER BY year
+	    SELECT faa_identifier,name,latitude,longitude
+		FROM ` + "`bigquery-public-data.faa.us_airports`" + `
+		LIMIT 10
 	`)
 	it, err := q.Read(ctx)
 	if err != nil {
 		fmt.Println(err) // TODO: Handle error.
 	}
+	var ap pb.Airport
+	var aps []*pb.Airport
 	for {
-		var values []bigquery.Value
-		err := it.Next(&values)
+		err := it.Next(&ap)
 		if err == iterator.Done {
 			break
 		}
 		if err != nil {
 			fmt.Println(err)
 		}
-		fmt.Println(values)
+		aps = append(aps, &ap)
 	}
+	fmt.Println(aps)
+	fmt.Printf("Received: %v", in.GetCode())
+	// var a pb.Airport
+	// for _, ap := range(values) {
+	// 	err = json.Unmarshal(ap, &a)
+	// 	aps = append(aps, )
+	// }
+	return &pb.Airports{
+		Airports: aps,
+	}, nil
+}
 
+func testGetData() {
+	ctx := context.Background()
+	client, err := bigquery.NewClient(ctx, projectID)
+	if err != nil {
+		fmt.Println(err)
+	}
+	q := client.Query(`
+	    SELECT faa_identifier,name,latitude,longitude
+		FROM ` + "`bigquery-public-data.faa.us_airports`" + `
+		LIMIT 10
+	`)
+	it, err := q.Read(ctx)
+	if err != nil {
+		fmt.Println(err) // TODO: Handle error.
+	}
+	var values []pb.Airport
+	var ap pb.Airport
+	for {
+		err := it.Next(&ap)
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			fmt.Println(err)
+		}
+		// fmt.Println(values)
+		values = append(values, ap)
+
+	}
+	fmt.Println(values)
+}
+
+func main() {
 	fmt.Println("vim-go")
+	// testGetData()
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		fmt.Printf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterFAAAirportDataServer(s, &server{})
+	fmt.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		fmt.Printf("failed to serve: %v", err)
+	}
 }
